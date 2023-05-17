e05eb014734d07df44c3ff054fa0697f21a0097b
SpecialTea99 <117664179+SpecialTea99@users.noreply.github.com>
2023-05-17 14:47:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Calcultor - Currency
+
+function calcultor() {
+  let amount = document.getElementById('amount').value;
+  let currency = document.getElementById('currency').value;
+
+  alert (currency * amount);
+
+}
